76469968995bad1e287f3c7541a6a7b32cbcb6d5
sid2290 <sudhanshunarang90@gmail.com>
2016-08-27 13:44:19 -0600

    Hoisting concept

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var a = 'Helloworld';
+//Hoisting : JS engine move variables and function up but in this code 
+// Why a is undefined?
+//Calling function and variable move up by JS engine to allocate memory
+// in this process functions work good but variables become undefined
+// so it is not good idea to rely on hoisting
+b();
+var a="Hello world";
+
+console.log(a);  
+
+
 
 function b() {
     console.log('Called b!');
 }
 
-//Calling function
-b();
-console.log(a);
