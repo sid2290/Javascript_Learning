852fbfb1ceb2b8fc791b0e57bf43540c037958aa
sid2290 <sudhanshunarang90@gmail.com>
2016-08-27 14:02:09 -0600

    undefined in js and value before and during execution of code.

--- app.js
+++ app.js
@@ -3,14 +3,29 @@
 //Calling function and variable move up by JS engine to allocate memory
 // in this process functions work good but variables become undefined
 // so it is not good idea to rely on hoisting
-b();
-var a="Hello world";
-
-console.log(a);  
 
 
+// remove comments to see the Javascript undefined property, always rememeber that if you forgot
+// to define variable js will automatically give it a value of undefined
+
+var a;
+//var a = "hello";
+
+if(a===undefined){
+    console.log('a is undefined');
+}
+else {
+    console.log('a is defined')
+}
 
 function b() {
     console.log('Called b!');
 }
 
+b();
+
+console.log(a);  // here a is undefined because this value is give to it before execution
+
+var a = "hello";
+
+console.log(a); // here a is undefined because this value is give to it during execution
