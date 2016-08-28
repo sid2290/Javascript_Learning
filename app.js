21d588ac09e4df4cbab44140ffaa23b4587fcaba
sid2290 <sudhanshunarang90@gmail.com>
2016-08-27 21:41:50 -0600

    Asynchronous issue 1

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
+function waitThreeSeconds() {
+    var ms =3000 + new Date().getTime();
+    while(ms> new Date()){};
+    console.log('Finished Function');
+}
+document.addEventListener('click', clickHandler);
 
- // single thread: one command is executed on time
-// synchronous: one line at a time in order
-
-function b() { 
-    console.log(myVar); // powerful concept of looking in outer enviroment (which is generally where code is sitting physically) 
-                        // But javscript keep on looking at it till global execution level untill it will find it
+function clickHandler () {
+    console.log("Clicked");
 }
 
-function a () {
-    
-    function c () {
-        console.log(myVar);
-    }
-    
-    var myVar= 2; // Second this myVar is of excution context a()
-    b();
-    c();
-   // console.log(myVar);
-    
-    
-}
-
-var myVar = 1; // First this seperate myVar of Global execution context 
-a();
-//console.log(myVar);
+waitThreeSeconds();
+console.log("Execution finished & Starting Checking click event");
