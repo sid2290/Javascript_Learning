9e9eb03b6be7010c0c81a8cb37a14fee698040ee
sid2290 <sudhanshunarang90@gmail.com>
2016-08-27 20:56:10 -0600

    Check how Execution context for js is created and it is totally different as compared to other lang. First read code, call next funtion in new context  and so on

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-//Hoisting : JS engine move variables and function up but in this code 
-// Why a is undefined?
-//Calling function and variable move up by JS engine to allocate memory
-// in this process functions work good but variables become undefined
-// so it is not good idea to rely on hoisting
 
-
-// remove comments to see the Javascript undefined property, always rememeber that if you forgot
-// to define variable js will automatically give it a value of undefined
-
-var a;
-//var a = "hello";
-
-if(a===undefined){
-    console.log('a is undefined');
-}
-else {
-    console.log('a is defined')
-}
+ // single thread: one command is executed on time
+// synchronous: one line at a time in order
 
 function b() {
-    console.log('Called b!');
+    var myVar; // Finally this is of myVar as undefined 
+    console.log(myVar);
+    
 }
 
-b();
+function a () {
+    var myVar= 2; // Second this myVar is of excution context a()
+    b();
+    console.log(myVar);
+    
+    
+}
 
-console.log(a);  // here a is undefined because this value is give to it before execution
-
-var a = "hello";
-
-console.log(a); // here a is undefined because this value is give to it during execution
+var myVar = 1; // First this seperate myVar of Global execution context 
+a();
+console.log(myVar);
