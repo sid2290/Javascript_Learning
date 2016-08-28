08d5ed0a1fb1437b3a22d06065db8e1c424f2c4d
sid2290 <sudhanshunarang90@gmail.com>
2016-08-27 21:13:15 -0600

    Concept of scope chain

--- app.js
+++ app.js
@@ -2,20 +2,25 @@
  // single thread: one command is executed on time
 // synchronous: one line at a time in order
 
-function b() {
-    var myVar; // Finally this is of myVar as undefined 
-    console.log(myVar);
-    
+function b() { 
+    console.log(myVar); // powerful concept of looking in outer enviroment (which is generally where code is sitting physically) 
+                        // But javscript keep on looking at it till global execution level untill it will find it
 }
 
 function a () {
+    
+    function c () {
+        console.log(myVar);
+    }
+    
     var myVar= 2; // Second this myVar is of excution context a()
     b();
-    console.log(myVar);
+    c();
+   // console.log(myVar);
     
     
 }
 
 var myVar = 1; // First this seperate myVar of Global execution context 
 a();
-console.log(myVar);
+//console.log(myVar);
