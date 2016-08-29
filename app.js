9fb457ace23863fc718c46f6837f21d485de1506
sid2290 <sudhanshunarang90@gmail.com>
2016-08-29 13:09:34 -0600

    Making sure that names are not colliding in the js files.

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-function setDefaultValue (name ) {
-
-	name = name || "Nick Name: Sid";
-	console.log(name);
-}
-
-setDefaultValue();
-setDefaultValue("Real name : Sudhanshu")
+console.log(libraryName);
---
+++ lib2.js
@@ -0,0 +1,2 @@
+// checking that names are not colliding in library
+window.libraryName = window.libraryName || "lib 2"; // instead of this var libraryName = "lib2";
