376315530eb8cb18b2246846abe4e2a0ccd2b2f7
sid2290 <sudhanshunarang90@gmail.com>
2016-09-04 13:41:29 -0600

    issue #7

--- app.js
+++ app.js
@@ -1,81 +1,22 @@
-// creating an object
+Simple();
 
-var person = new Object();
-
-// Creating object properties
-person ["firstname"] = "Sudhanshu";
-person ["lastname"]  = "Nanarg";
-
-// Assigning variable to name of property
-var firstNameProperty = "firstname";
-
-console.log(person);
-//one way to call property of object
-console.log(person[firstNameProperty]);
-
-// Use of dot operator call property on object
-console.log(person.firstname);
-
-person.address = new Object();
-
-person.address.street = "vanier st.";
-person.address.city ="Montreal";
-person.address.state="QC";
-
-console.log(person.address);
-console.log(person.address.city);
-
-person.address.Details =new Object();
-person.address.Details.married = "No";
-person.address.Details.phonenumber ="xxxxxxxx803";
-
-console.log(person.address.Details);
-console.log(person.address.Details.married)
-
-// or Instead of using new operators you can use object literals
-
-var literalPerson = {
-    Firstname:'Sudhanshu',
-    Lastname:'Narang',
-    address: {
-        street: "vanier st.",
-        city:"Montreal"
-    }
+function Simple() {
+    console.log("Simple function");
 }
 
-console.log(literalPerson);
-console.log(literalPerson.Firstname);
 
-//passing objects in functions
-function greet(person) {
-    console.log("hello " + person.flyObjectfirstName);
-};
-
-greet(literalPerson);
-
-// creating objects on the fly
-
-greet({
-    flyObjectfirstName: 'Sid',
-    flyObjectlastName:'Narang'
-});
-
-//Faking NameSpace
-var nameSpaceGreet ='Hello!';
-var nameSpaceSGreet ='Hola!';
-
-var english = {};
-var french = {};
-english.nameSpaceGreet = 'Hello!';
-french.nameSpaceGreet = 'Salute!';
-console.log(english.nameSpaceGreet);
-console.log(french.nameSpaceGreet);
-
-//Coverting JSON to JS object and JS object to JSON
-var objectLiteral = {
-    firstname: 'Mary',
-    isAProgrammer: true
+var anonymousFunction = function(){
+    console.log("Anonymous Function");
 }
 
-console.log(JSON.parse('{"firstname":"Mary","isAProgrammer":true}'));
-console.log(JSON.stringify(objectLiteral));
+anonymousFunction();
+
+function ArgumentTaking(a) {
+    console.log(a);
+}
+
+//passing object as argument
+ArgumentTaking({greeting: "hello"});
+
+//passing function as argument
+ArgumentTaking(function() {console.log("Argument function code")});
