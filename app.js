8282007d439a034bf5a0da9d457df6c466b5c10f
sid2290 <sudhanshunarang90@gmail.com>
2016-09-06 10:15:56 -0600

    issue #9

--- app.js
+++ app.js
@@ -1,57 +1,19 @@
-// Using this keyword 
-//Trying to use "this" keyword in the context of simple function
+// Javascript array
 
-Simple();
-
-function Simple() {
-    console.log(this);
-    this.something="Hello";
-    console.log(something);
-}
-
-
-var anonymousFunction = function(){
-    console.log(this);
-}
-
-anonymousFunction();
-
-
-//Showing that something variable is undefined for the context of Simple function
-console.log(Simple.something);
-
-//but, "this" keyword is pointing to the global window object
-console.log(window.something);
-
-
-function OtherSimple () {
-    console.this(this);
-    
-   }; 
-
-var JS = {
-    name: 'The JavaScript Object',
-    log: function () {
-        var self = this;
-        self.name="updated name";
-        console.log(this);
-        var setname = function(newname) {
-            self.name=newname;
-            console.log(self);
-        };
-        setname("update again!!");
-        console.log(self);
-    }
-}
-
-JS.log();
-
-OtherSimple.othersomething = {
-    name: 'Function in Function',
-    log: function () {
-        console.log(this);
-    }
-}
-
-OtherSimple.othersomething.log();
-
+var arr = [
+           1,
+           false,
+           {
+               Car:"Altima",
+               Brand:"Nissan"
+           },
+            function start() {
+                console.log("Car Started");
+            },
+            "Awesome"
+          ];
+console.log(arr);
+// calling function from the array
+arr[3]();
+// using object inside the array
+console.log(arr[2].Car);
