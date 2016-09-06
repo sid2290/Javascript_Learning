7cb89f5693b337ee257e5083e2b95ee9640b063d
sid2290 <sudhanshunarang90@gmail.com>
2016-09-06 10:39:31 -0600

    issue #10

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// Javascript array
+//How argument keyword is use
 
-var arr = [
-           1,
-           false,
-           {
-               Car:"Altima",
-               Brand:"Nissan"
-           },
-            function start() {
-                console.log("Car Started");
-            },
-            "Awesome"
-          ];
-console.log(arr);
-// calling function from the array
-arr[3]();
-// using object inside the array
-console.log(arr[2].Car);
+function car(name,brand,color) {
+    console.log(arguments);
+    console.log(name);
+    console.log(brand);
+    console.log(color);
+    console.log("-----------------------------");    
+}
+
+car();
+car("Altima");
+car("Altima","Nissan");
+car("Altima","Nissan","White");
+
