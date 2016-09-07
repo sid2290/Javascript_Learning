5065520edd037fa5dcbf65fef03404862c27ade5
sid2290 <sudhanshunarang90@gmail.com>
2016-09-07 12:58:29 -0600

    issue #13

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 //creating a function
-function greet() {
-    console.log("Hello");
+function greet(name) {
+    return function () {
+    console.log("Hello " + name);
+    }
 } ;
 
 //Calling a function
-greet();
-
-//creating a function and assigning it to variable
-var greeting = function () {
-    console.log("Hello again!!");
-};
-
-//Variable becomes function and we can invoke it
-greeting();
-
-//Now Using IIFE to assign variable a string value rather that function
-
-var iifeGreeting = function() {
-    return "hello";
-}();
-
-//writing value of variable on console
-console.log(iifeGreeting);
-var name="Safe outside";
-// using IIFE stand alone
-(function() {
-    var name="safe inside";
-    console.log("hello "+ name);
-}());
-
-console.log(name);
+var outer =greet("Sid");
+console.log(outer);
+outer();
