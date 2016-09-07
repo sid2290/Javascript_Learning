e1952975e865084b5ec79510afa48930c1ec2218
sid2290 <sudhanshunarang90@gmail.com>
2016-09-07 13:41:09 -0600

    issue #13

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 //creating a function
-function greet(name) {
-    return function () {
-    console.log("Hello " + name);
-    }
+function Closure() {
+    var arr =[];
+    for (var i =0 ; i<3; i++){
+    arr.push(function () {    
+            console.log(i);
+    });
+    };
+    return arr;
 } ;
 
 //Calling a function
-var outer =greet("Sid");
-console.log(outer);
-outer();
+var outer = Closure();
+outer[0]();
+outer[1]();
+
+function Closure2() {
+    var arr =[];
+    for (var i =0 ; i<3; i++){
+    arr.push((function (j) {
+        return function() {
+    console.log(j);
+        }
+    }(i)));
+    };
+    return arr;
+} ;
+
+//Calling a function
+var outer2 = Closure2();
+outer2[0]();
+outer2[1]();
