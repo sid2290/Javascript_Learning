23c24b612ed43663d2455e01a8b710069e639277
sid2290 <sudhanshunarang90@gmail.com>
2016-09-07 21:22:22 -0600

    issue #15

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-//creating a function
-function Add(callback) {
-  var a = 10;
-    var b= 20;
-    var c= a+b;
-    console.log(c);
-    callback();
-} ;
+var person = {
+    firstname:"Sid",
+    lastname:"Narang",
+    log: function () {
+        console.log(this.firstname + " " + this.lastname);
+    }
+}
 
-Add(function () {console.log("Adding is finished finally")});
+person.log();
+
+var log1 = (function(arg1,arg2) {
+    console.log(this.firstname);
+    
+}).bind(person);
+log1();
+
+var log2 = (function(arg1,arg2) {
+    console.log(this.firstname+ " " + arg1 + " " + arg2);
+    
+}).call(person,1,2);
+
+
+var log3 = (function(arg1,arg2) {
+    console.log(this.firstname+ " " + arg1 + " " + arg2);
+    
+}).apply(person,[1,2]);
+
+
+
+
 
