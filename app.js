8e5fd105e3493bd9b63599b3dd2689713448c948
sid2290 <sudhanshunarang90@gmail.com>
2016-09-13 15:34:50 -0600

    Prototype #17

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
-function PassFirstClassFunctions(arr,fn) {
-    
-    var arr2 = [] ;
-    
-    for(var i=0;i<arr.length;i++) {
-         arr2[i] = fn(arr[i]);
+var person = {
+    firstname : 'sid',
+    lastname : 'narang',
+    getFullName : function () {
+        return this.firstname + " " + this.lastname;
     }
-    
-    return arr2;
 }
 
-var array_1 = [1,2,3];
-
-var array_2 = PassFirstClassFunctions(array_1,function (x) {
-    return x *2;
-})
-
-var array_3 = PassFirstClassFunctions(array_1,function (x) {
-    return x *3;
-})
-console.log(array_2 );
-console.log(array_3 );
-
-var checkLimit = function (x,limit) {
-    return x < limit;
+var john = {
+    firstname : 'John',
+    lastname: 'desuza'
 }
+// Use of __proto__
 
-var array_4 = PassFirstClassFunctions(array_1,checkLimit.bind(this,1));
-console.log(array_4 );
+john.__proto__ = person;
 
-//using underscore library
-var array_5 = _.map(array_1,function(x) {return x*3});
-
-console.log(array_5);
-
+console.log(john.getFullName());
