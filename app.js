5875599428c8db9c0eb27e13448b31fba5aa25b0
sid2290 <sudhanshunarang90@gmail.com>
2016-10-04 16:11:01 -0600

    creating empty object, function and array. issue #18

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-var person = {
-    firstname : 'sid',
-    lastname : 'narang',
-    getFullName : function () {
-        return this.firstname + " " + this.lastname;
-    }
-}
+// creating three things object, empty function and empty array
 
-var john = {
-    firstname : 'John',
-    lastname: 'desuza'
-}
-// Use of __proto__ for inheritance (inheriting john object from person)
-
-john.__proto__ = person;
-
-// Calling property by inheritance 
-
-console.log(john.getFullName());
-
-var jane = {
-  firstname : 'Jane'  
-};
-
-// inheriting jane object from person
-
-jane.__proto__ = person;
-
-console.log(jane.firstname + " " + jane.lastname);
+var a = {};
+var b = function() { };
+var c = [];
