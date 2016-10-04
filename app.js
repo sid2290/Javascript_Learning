199fd3e8770c4af56af1eeddebf325f503d03881
sid2290 <sudhanshunarang90@gmail.com>
2016-10-04 17:03:05 -0600

    use of extend and reflection issue #18

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
-// creating three things object, empty function and empty array
+var person = {
+    firstname : 'sid',
+    lastname : 'narang',
+    getFullName : function () {
+        return this.firstname + " " + this.lastname;
+    }
+}
 
-var a = {};
-var b = function() { };
-var c = [];
+var john = {
+    firstname : 'John',
+    lastname: 'desuza'
+}
+// Use of __proto__ for inheritance (inheriting john object from person)
+
+john.__proto__ = person;
+
+// Calling property by inheritance 
+
+console.log(john.getFullName());
+
+var jane = {
+  firstname : 'Jane'  
+};
+
+// inheriting jane object from person
+
+jane.__proto__ = person;
+
+console.log(jane.firstname + " " + jane.lastname);
+
+// for each loop
+for (prop in john) {
+    if(john.hasOwnProperty(prop)){
+    console.log(prop + ':' + john[prop]);
+    }
+}
+
+var jane = {
+    address: '111 Main St.',
+    getFormalFullName: function() {
+        return this.lastname + ',' + this.firstname; 
+    }
+}
+
+var jim = {
+    getFirstName: function() {
+        return firstname;
+    }
+}
+
+_.extend(john,jane,jim);
+
+console.log(john);
