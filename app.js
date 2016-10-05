89097bbef63ea789bf6bfe48a31ecc8811f8fe52
sid2290 <sudhanshunarang90@gmail.com>
2016-10-04 23:42:20 -0600

    Arrays issue # 9

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-// using prototype to create method on built in stuff
-String.prototype.isLengthGreaterThan = function(limit) {
-    return this.length > limit;
+var arr = ['John', 'Jane','Jim'];
+
+for (var index in arr) {
+    console.log(index + ':' + arr[index]);
 }
-
-//converting string automatically to object
-console.log("John".isLengthGreaterThan(3));
-
-//converting number to object is not possible automatically
-Number.prototype.isPositive = function() {
-    return this > 0;
-}
-
-var a = new Number(3);
-console.log(a.isPositive());
