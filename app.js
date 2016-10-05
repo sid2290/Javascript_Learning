a677689abe2fef9c46233387cd919fb788662ae2
sid2290 <sudhanshunarang90@gmail.com>
2016-10-04 18:24:54 -0600

    constructors in javascript issue #19

--- app.js
+++ app.js
@@ -1,53 +1,21 @@
-var person = {
-    firstname : 'sid',
-    lastname : 'narang',
-    getFullName : function () {
-        return this.firstname + " " + this.lastname;
-    }
+// create objects in javascript
+
+function Person(firstname, lastname) {
+    
+    console.log(this);
+    this.firstname = firstname;
+    this.lastname = lastname;
+    console.log('This function is invoked');
+    
+   // return { greeting: 'I got in the way'};
 }
 
-var john = {
-    firstname : 'John',
-    lastname: 'desuza'
-}
-// Use of __proto__ for inheritance (inheriting john object from person)
-
-john.__proto__ = person;
-
-// Calling property by inheritance 
-
-console.log(john.getFullName());
-
-var jane = {
-  firstname : 'Jane'  
-};
-
-// inheriting jane object from person
-
-jane.__proto__ = person;
-
-console.log(jane.firstname + " " + jane.lastname);
-
-// for each loop
-for (prop in john) {
-    if(john.hasOwnProperty(prop)){
-    console.log(prop + ':' + john[prop]);
-    }
-}
-
-var jane = {
-    address: '111 Main St.',
-    getFormalFullName: function() {
-        return this.lastname + ',' + this.firstname; 
-    }
-}
-
-var jim = {
-    getFirstName: function() {
-        return firstname;
-    }
-}
-
-_.extend(john,jane,jim);
+// creating objects in javascript
+var john = new Person("Sid","Narang");
 
 console.log(john);
+
+//creating other object
+var jane = new Person('Sudhanshu','Narang');
+
+console.log(jane);
